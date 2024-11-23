ada53148ae5b29e52bb72009ef8f7424786e9cf0
Toan bach <ngoctoan@live.com>
2024-11-23 10:21:43 +0700

    Encapsulation

---
+++ EncapsulationSample/Program.cs
@@ -0,0 +1,33 @@
+﻿class BankAccount
+{
+    private decimal _balance;
+    public decimal Balance
+    {
+        get { return _balance; }
+        private set
+        {
+            if (value >= 0)
+                _balance = value;
+        }
+    }
+
+    public BankAccount(decimal initialBalance)
+    {
+        Balance = initialBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0) {
+            Balance += amount;
+        }
+    }
+
+    public bool Withdraw(decimal amount) {
+        if (amount > 0 && Balance >= 0) { 
+            Balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
