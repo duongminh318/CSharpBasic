2acaf4a8084589658dd75eb01cc05c9ca4d40aba
Toan bach <ngoctoan@live.com>
2024-11-26 06:26:48 +0700

    Math

---
+++ MathPractice1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter radius:");
+double radius = double.Parse(Console.ReadLine());
+double area = Math.PI * Math.Pow(radius, 2);
+Console.WriteLine($"Area of circle with radius: {radius} is {area}");
---
+++ MathSample/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine($"Số PI: {Math.PI}");
+Console.WriteLine($"Số e: {Math.E}");
+
+Console.WriteLine($"Giá trị tuyệt đối của -10: {Math.Abs(-10)}");
+Console.WriteLine($"Làm tròn lên 3.1: {Math.Ceiling(3.1)}");
+Console.WriteLine($"Làm tròn xuống 3.9: {Math.Floor(3.9)}");
+Console.WriteLine($"Làm tròn 3.5: {Math.Round(3.5)}");
+
+//Lũy thừa/căn bậc 2
+
+Console.WriteLine($"2^3: {Math.Pow(2,3)}");
+Console.WriteLine($"Căn bậc 2 của 16: {Math.Sqrt(16)}");
+
+//Lượng giác
+double radians = Math.PI / 4;
+Console.WriteLine($"Sin (45): {Math.Sin(radians)}");
+Console.WriteLine($"Cos (45): {Math.Cos(radians)}");
+Console.WriteLine($"Tag (45): {Math.Tan(radians)}");
+
+//Logarit
+Console.WriteLine($"Log tự nhiên của e: {Math.Log(Math.E)}");
+Console.WriteLine($"Log cơ số 10 của 100: {Math.Log10(100)}");
